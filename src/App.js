53c3d723aeba535c79cccf1a363193bb2f8ca753
Liau Jian Jie <liaujianjie@gmail.com>
2021-03-13 16:25:32 +0800

    Clean up stains

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ export default function App() {
   const audioRef = useRef(null);
   const [choochoo, updateChoochoo] = useState(false);
 
+  // Start physics on mount.
   useEffect(() => {
     startPhysics();
   }, []);
 
+  // Shoot jizzho on keypress.
   useEffect(() => {
-    // document.getElementById("App").addEventListener("click", jizzho);
     window.addEventListener("keypress", jizzho);
     return () => {
-      // document.getElementById("App").removeEventListener("click", jizzho);
       window.removeEventListener("keypress", jizzho);
     };
   });
 
+  // Shoot jizzho on every beat.
   useEffect(() => {
     if (!choochoo) {
       return;
@@ -40,21 +41,14 @@ export default function App() {
           // audioRef.current.pause();
         }
       }}
-      onMouseDown={() => {
-        //..
-      }}
-      onMouseUp={() => {
-        //..
-      }}
     >
       <img
         className="choo-choo-motherfucker"
         src={choochoo ? "/jiho-choo-choo.jpg" : undefined}
-        // style={{ opacity: choochoo ? 1 : 0 }}
       />
-      {/* <div className="thanku">Thank u for being an amazing friend</div> */}
       {/* preload jizzho face */}
       <img src="/jizzho.png" style={{ opacity: 0 }} />
+      <img src="/jiho-choo-choo.jpg" style={{ opacity: 0 }} />
       <audio ref={audioRef} src="/thomas-choo-choo.mp3" loop />
     </div>
   );
